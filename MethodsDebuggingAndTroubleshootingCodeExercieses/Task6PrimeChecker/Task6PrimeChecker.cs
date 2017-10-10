96e479be1c468bd1dfb7e6df2faab67074fa5165
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-10 20:44:34 +0300

    task6

--- MethodsDebuggingAndTroubleshootingCodeExercieses/Task6PrimeChecker/Task6PrimeChecker.cs
+++ MethodsDebuggingAndTroubleshootingCodeExercieses/Task6PrimeChecker/Task6PrimeChecker.cs
@@ -3,8 +3,27 @@ namespace Task6PrimeChecker
 {
     class Task6PrimeChecker
     {
+        static void prime(long pr)
+        {
+            bool primeN = true;
+            if (pr<2)
+            {
+                primeN = false;
+            }
+            for (long i = 2; i <= Math.Sqrt(pr); i++)
+            {
+                if (pr % i == 0)
+                {
+                    primeN=false;
+                    break;
+                }
+            }
+            Console.WriteLine(primeN);
+        }
         static void Main()
         {
+            long prem = long.Parse(Console.ReadLine());
+            prime( prem);
         }
     }
 }
