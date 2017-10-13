648881f93526a5c46013bc701df14127f93dc8dd
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-13 18:01:53 +0300

    update laptop

---
+++ ListLab/Task1RemoveNegativesAndReverse/Task1RemoveNegativesAndReverse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1RemoveNegativesAndReverse
+{
+    class Task1RemoveNegativesAndReverse
+    {
+        static void Main()
+        {
+            List<int> input = int.ParseConsole.ReadLine().Split(' ').Select(int.Parse).ToList();
+            input.Sort();
+            Console.WriteLine(string.Join(" ",input));
+        }
+    }
+}
---
+++ ListLab/testLIst ov char/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testLIst_ov_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "fgh 12 kjh 85 lih";
+
+            char[] chars = new char[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                chars[i] = s[i];
+            }
+        }
+    }
+}
