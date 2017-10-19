bd25eb4ed7da69d3a21c11092f8a7cb6a91ddf33
YankoGrancharov <Ygrancharov@gmail.com>
2017-10-19 12:17:26 +0300

    task 1

--- ListsExercises/Task1MaxSequenceOfEqualElements/Task1MaxSequenceOfEqualElements.cs
+++ ListsExercises/Task1MaxSequenceOfEqualElements/Task1MaxSequenceOfEqualElements.cs
@@ -10,7 +10,37 @@ namespace Task1MaxSequenceOfEqualElements
     {
         static void Main()
         {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int count = 1;
+            int num = 0;
+            int maxCount = 0;
 
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                    
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        num = numbers[i];
+                    }
+
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            for (int j = 0; j < maxCount; j++)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
         }
     }
 }
