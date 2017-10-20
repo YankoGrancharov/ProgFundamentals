395f3e3f657f7a07e52a8d70bdc42d30dadae64a
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-20 17:31:40 +0300

    w

---
+++ ListsExercises/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BombNumbers
+{
+    public class BombNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numOrder = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            bomb(numOrder);
+        }
+
+        private static void bomb(List<int> numOrder)
+        {
+            List<int> area = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int specialNumber = area[0];
+            int pover = area[1];
+
+            List<int> indexes = new List<int>();
+
+            for (int i = 0; i < numOrder.Count; i++)
+            {
+                if (numOrder[i] == specialNumber)
+                {
+                    indexes.Add(i);
+                    foreach (int indx in indexes)
+                    {
+                        int start = 0;
+                        int end = 0;
+                        if (indx - pover > 0)
+                        {
+                            start = indx - pover;
+                        }
+
+                        if (indx + pover > numOrder.Count - 1)
+                        {
+                            end = numOrder.Count - 1;
+                        }
+                        else
+                        {
+                            end = indx + pover;
+                        }
+
+                        for (int l = start; l <= end; l++)
+                        {
+                            numOrder[l] = 0;
+                        }
+                    }
+                }
+
+            }
+
+            int sum = numOrder.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
--- ListsExercises/Task1MaxSequenceOfEqualElements/Task1MaxSequenceOfEqualElements.cs
+++ ListsExercises/Task1MaxSequenceOfEqualElements/Task1MaxSequenceOfEqualElements.cs
@@ -10,6 +10,19 @@ namespace Task1MaxSequenceOfEqualElements
     {
         static void Main()
         {
+            List<int> input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int counter = 1;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i]==input[i+1])
+                {
+                    counter++;
+                }
+
+            }
 
         }
     }
