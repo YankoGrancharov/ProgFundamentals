b2b4efe96828cbcba616c4b6a7a820924ae5696d
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-22 08:14:22 +0300

    new

---
+++ FilesAndExeptions/Task1oddLines/Task1OddLines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1oddLines
+{
+    class Task1oddLines
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+            List<string> OddLinex = new List<string>();
+            for (int i = 1; i < lines.Length; i+= 2)
+            {
+
+                File.AppendAllText("OddLines.txt");
+
+            }
+            File.WriteAllLines("OddLines.txt");
+        }
+    }
+}
