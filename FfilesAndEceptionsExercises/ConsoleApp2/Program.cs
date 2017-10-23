3cf398526d532b1ce6895f3cce146d3b0b292c21
Yanko Grancharov <YGrancharov@gmail.com>
2017-10-23 06:14:29 +0300

    k

---
+++ FfilesAndEceptionsExercises/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace IndexOfLetters
+{
+    public class IndexOfLetters
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "../../input.txt";
+            string outpulFile = "../../output.txt";
+
+            char[] let = File.ReadAllText(inputFile).ToLower().ToCharArray();
+
+            positia(let);
+        }
+
+        private static void positia(char[] let)
+        {
+            Dictionary<char, int> leterandP = new Dictionary<char, int>();
+
+            int pos = 0;
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (!leterandP.ContainsKey(i))
+                {
+                    leterandP.Add(i, pos);
+                }
+                pos++;
+            }
+
+            foreach (char c in let)
+            {
+                if (leterandP.ContainsKey(c))
+                {
+                    var output = ($"{c} -> {leterandP[c]}" + Environment.NewLine);
+                    File.AppendAllText(outpulFile, output);
+                }
+            }
+        }
+    }
+}
---
+++ FfilesAndEceptionsExercises/ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MostFrequentNumber
+{
+    public class MostFrequentNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] num = File.ReadAllText("../../input.txt").Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            numFind(num);
+
+        }
+
+        private static void numFind(int[] num)
+        {
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+
+            foreach (int n in num)
+            {
+                if (!nums.ContainsKey(n))
+                {
+                    nums.Add(n, 0);
+                }
+                nums[n] += 1;
+            }
+
+            int mostCommonValue = 0;
+            int highestCount = 0;
+
+            foreach (KeyValuePair<int, int> pair in nums)
+            {
+                if (pair.Value > highestCount)
+                {
+                    mostCommonValue = pair.Key;
+                    highestCount = pair.Value;
+                }
+            }
+            File.AppendAllText("../../output.txt",$"{mostCommonValue}" );
+        }
+    }
+}
